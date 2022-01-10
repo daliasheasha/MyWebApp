f847d4962c654a2dfa866b0220a6607536756e13
Dalia Abo Sheasha <daliasheasha@gmail.com>
2022-01-10 16:49:35 -0600

    The application using javax namespace

--- src/main/java/com/jetbrains/MyWebApp/MyServlet.java
+++ src/main/java/com/jetbrains/MyWebApp/MyServlet.java
@@ -1,12 +1,12 @@
 package com.jetbrains.MyWebApp;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
