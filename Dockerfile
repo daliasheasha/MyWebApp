3b69448b54827b4eb94f84f3d99e0786bfefc31b
Dalia Abo Sheasha <daliasheasha@gmail.com>
2022-01-10 16:14:11 -0600

    Update to Jakarta Persistence

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM tomcat:10-jdk11
+FROM tomcat:10-jdk17
 ADD target/MyWebApp.war /usr/local/tomcat/webapps/MyWebApp.war
 EXPOSE 8080
 CMD ["catalina.sh", "run"]
--- src/main/java/com/jetbrains/MyWebApp/MyServlet.java
+++ src/main/java/com/jetbrains/MyWebApp/MyServlet.java
@@ -1,8 +1,8 @@
 package com.jetbrains.MyWebApp;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
